00a2b633dd916b465c7441a1319f55ac10188345
zulqarnain <zulqarnain.hidayat@yahoo.com>
2020-12-12 17:54:54 +0700

    ..

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -31,24 +31,18 @@
           </div>
         </section>
       </div>
-<<<<<<< Updated upstream
       <!-- <footer class="main-footer">
         <div class="footer-left">
-=======
       <footer class="main-footer">
-        <!-- {{-- <div class="footer-left">
->>>>>>> Stashed changes
+        {{-- <div class="footer-left">
           Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
         </div>
         <div class="footer-right">
           2.3.0
-<<<<<<< Updated upstream
         </div>
       </footer> -->
-=======
         </div> -->
       </footer>
->>>>>>> Stashed changes
     </div>
   </div>
 
